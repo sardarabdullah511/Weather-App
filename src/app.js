eaf83d2c550f3da04b3f4c10349a0736353ae28f
Sardar Abdullah <sardarabdullah511@gmail.com>
2023-11-22 08:44:23 +0500

    new

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const forecast = require('./utils/forecast')
 const { error } = require('console')
 const app = express()
 
-// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
